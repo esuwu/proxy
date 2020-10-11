e72f28368318c7738f4223a09d0cab5eae6c4473
Alexandr Dolgavin <adolgavin@web3tech.ru>
2020-10-11 19:48:17 +0300

    change params

--- monitoring/monitoring.go
+++ monitoring/monitoring.go
@@ -4,16 +4,16 @@ import "github.com/prometheus/client_golang/prometheus"
 
 var (
 	RequestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    "codehub_services_requestDuration",
+		Name:    "adolgavinBackend1_services_requestDuration",
 		Buckets: prometheus.LinearBuckets(0.01, 0.01, 10),
 	}, []string{"path", "method"})
 
 	DBQueryDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    "codehub_services_SqlDuration",
+		Name:    "adolgavinBackend1_services_SqlDuration",
 		Buckets: prometheus.LinearBuckets(0.01, 0.01, 10),
 	}, []string{"rep", "method"})
 
 	Hits = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "codehub_services_hits",
+		Name: "adolgavinBackend1_services_hits",
 	}, []string{"status", "path"})
 )
